69667279efe99e5b7e9cdc9f72dbdeaaafa251c9
Fernando Mendes <fernando.mendes91@outlook.com>
2021-01-04 20:04:46 -0300

    Finalização do projeto todo-app

--- todo-app/frontend/src/todo/TodoList.jsx
+++ todo-app/frontend/src/todo/TodoList.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import IconButton from '../template/IconButton';
 
-const TodoList = () => {
+const TodoList = props => {
+
+    const renderRows = _ => {
+        const list = props.list || []
+
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone' : ''}>
+                    {todo.description}
+                </td>
+                <td>
+                    <IconButton style="success" icon="check" hide={todo.done} onClick={_ => props.handleMarkAsDone(todo)} />
+                    <IconButton style="warning" icon="undo" hide={!todo.done} onClick={_ => props.handleMarkAsPending(todo)} />
+                    <IconButton style="danger" icon="trash-o" hide={!todo.done} onClick={_ => props.handleRemove(todo)} />
+                </td>
+            </tr>
+        ))
+
+    }
+
+
     return (
-        <div>
-            <h1>List</h1>
-        </div>
+        <table className="table">
+            <thead>
+                <tr >
+                    <th>Descrição</th>
+                    <th className="tableActions">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
     );
 }
 
